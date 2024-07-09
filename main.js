f469b996dcc00aad056683a9c1862b765e63a862
Wuid0801 <pig10326@naver.com>
2024-07-09 23:21:16 +0900

    [fix] api 배포 수정

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const API_KEY = "391ef86b614f41ef954d2891f97dcc27"
 let news = [];
 const getLatestNews = async ()=>{
   
-  const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+  const url = new URL(`https://monumental-eclair-c31282.netlify.app/top-headlines`);
   const response = await fetch(url).then(response => response.json()) 
   .then(data => {
     console.log(data.articles); 
