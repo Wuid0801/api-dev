2d420596bda92763f14665aef7fa27f1e50f3873
Wuid0801 <pig10326@naver.com>
2024-07-09 23:07:57 +0900

    [fix] .then 사용

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const API_KEY = "391ef86b614f41ef954d2891f97dcc27"
 let news = [];
 const getLatestNews = async ()=>{
   
-  const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
-  const response = await fetch(url);
-  const data = response.json();
-  news = data.articles;
-  console.log("data:", news);
+  const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+  const response = await fetch(url).then(response => response.json()) 
+  .then(data => {
+    console.log(data.articles); 
+  })
+  .catch(error => {
+    console.error('Error:', error); 
+  });
+
 }
 getLatestNews();
