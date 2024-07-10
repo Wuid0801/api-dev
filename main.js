5720754c8f014cec2c26660357f7da04b58fcb9f
Wuid0801 <pig10326@naver.com>
2024-07-11 01:31:43 +0900

    [feat] 뉴스 추가

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const API_KEY = "391ef86b614f41ef954d2891f97dcc27"
-let news = [];
-const getLatestNews = async ()=>{
-  
-  const url = new URL(`https://monumental-eclair-c31282.netlify.app/top-headlines`);
-  const response = await fetch(url).then(response => response.json()) 
-  .then(data => {
-    console.log(data.articles); 
-  })
-  .catch(error => {
-    console.error('Error:', error); 
-  });
+let newsList = [];
+const getLatestNews = async () => {
 
+  const url = new URL(`https://monumental-eclair-c31282.netlify.app/top-headlines`);
+  const response = await fetch(url).then(response => response.json())
+    .then(data => {
+      console.log(data.articles);
+      newsList = data.articles;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+    render();
+}
+
+const render = () => {
+  const newsHTML = newsList.map(news => `<div class="row news">
+    <div class="col-lg-4">
+      <img class="new-img-size fixed-img img-fluid"
+        src="${news.urlToImage}"
+        alt="gom"
+      />
+    </div>
+    <div class="col-lg-8 ">
+      <h2>${news.title}</h2>
+      <p>${news.description}</p>
+      <div>${news.source.name} * ${news.publishedAt}</div>
+    </div>
+  </div>`).join('');
+
+  document.getElementById("news-board").innerHTML = newsHTML
 }
 getLatestNews();
