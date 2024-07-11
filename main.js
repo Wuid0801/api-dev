6748767778258bd5ad79e2fbcf4a2d7b314c55e8
Wuid0801 <pig10326@naver.com>
2024-07-11 13:15:38 +0900

    [feat] 카테고리 기능 추가
    1. 카테고리 검색 기능을 추가했습니다
    2. Summary를 200자 제한했습니다
    3. 내용, 사진, 출처가 없을 경우를 처리했습니다
    4. 날짜 표기에 momnet을 사용했습니다
    5. 모바일 카테고리 목록의 css를 수정했습니다

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const API_KEY = "391ef86b614f41ef954d2891f97dcc27"
 let newsList = [];
-const getLatestNews = async () => {
+const getLatestNews = async (category = '') => {
 
   const url = new URL(`https://monumental-eclair-c31282.netlify.app/top-headlines`);
+  url.searchParams.append('category', category);
   const response = await fetch(url).then(response => response.json())
     .then(data => {
       console.log(data.articles);
@@ -11,24 +12,52 @@ const getLatestNews = async () => {
     .catch(error => {
       console.error('Error:', error);
     });
-    render();
+  render();
 }
 
 const render = () => {
-  const newsHTML = newsList.map(news => `<div class="row news">
-    <div class="col-lg-4">
-      <img class="new-img-size fixed-img img-fluid"
-        src="${news.urlToImage}"
-        alt="gom"
-      />
-    </div>
-    <div class="col-lg-8 ">
-      <h2>${news.title}</h2>
-      <p>${news.description}</p>
-      <div>${news.source.name} * ${news.publishedAt}</div>
-    </div>
-  </div>`).join('');
+  const maxLength = 200;
+
+
+  const newsHTML = newsList.map(news => {
+    let description = news.description || '내용없음';
+    let imageUrl = news.urlToImage;
+    let sourceName = news.source.name || 'no source';
+
+    if (description.length > maxLength) {
+      description = description.substring(0, maxLength) + '...';
+    }
+
+    return `<div class="row news">
+      <div class="col-lg-4">
+        <img class="new-img-size fixed-img img-fluid"
+          src="${imageUrl}"
+          alt="gom"
+          onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU';"
+        />
+      </div>
+      <div class="col-lg-8 ">
+        <h2>${news.title}</h2>
+        <p>${description}</p>
+        <div>${sourceName ? `${sourceName} *` : "no source"} ${moment(news.publishedAt).fromNow()}</div>
+      </div>
+    </div>`;
+  }).join('');
 
   document.getElementById("news-board").innerHTML = newsHTML
 }
 getLatestNews();
+
+document.querySelector('.search-icon').addEventListener('click', function () {
+  const searchBox = document.querySelector('.search-box');
+  searchBox.classList.toggle('visible');
+});
+
+const categoryBtns = document.querySelectorAll('.category-btn');
+categoryBtns.forEach(btn => {
+  btn.addEventListener('click', function (event) {
+    event.preventDefault(); // 기본 동작 방지
+    const category = this.getAttribute('data-category'); // 클릭된 버튼의 카테고리 가져오기
+    getLatestNews(category); // 해당 카테고리의 뉴스 가져오기
+  });
+});
